4a2d4783d95c8e9306e08784d71dfc6e0f0fdc3f
Crxaw <48805031+sitescript@users.noreply.github.com>
2025-01-07 11:33:05 +0000

    Update index.tsx

--- src/equicordplugins/noDeleteSafety/index.tsx
+++ src/equicordplugins/noDeleteSafety/index.tsx
@@ -44,7 +44,7 @@ export default definePlugin({
         {
             find: ".DELETE,onClick(){let",
             replacement: {
-                match: /let \i=(\i).toString\(\)/,
+                match: /let \i=(\i).toString\((\d+)\)/,
                 replace: "$self.HandleGuildDeleteModal($1);return;$&"
             }
         }
